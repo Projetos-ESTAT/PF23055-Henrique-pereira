ebb6430afd9897d0b8510be4a1bfaacaffb5ed82
febap02 <felipe.bapcar@gmail.com>
2023-11-16 15:50:36 -0300

    Código da primeira análise da variavel SEXO + gráfico

--- rdocs/Febap.R
+++ rdocs/Febap.R
@@ -22,3 +22,35 @@ source("rdocs/source/packages.R")
 # de teste depreciados, ou ao menos deixando como comentário. Dê preferência
 # as funções dos pacotes contidos no Tidyverse para realizar suas análises.
 # ---------------------------------------------------------------------------- #
+
+##Análise da variável sexo:
+dados <- dados %>%
+  slice(-48)
+
+dados$SEXO <- dados$SEXO %>%
+  str_replace('F', 'Feminino') %>%
+  str_replace('M', 'Masculino')
+
+sexo <- dados %>%
+  filter(!is.na(SEXO)) %>%
+  count(SEXO) %>%
+  mutate(
+    freq = n %>% percent(),
+  ) %>%
+  mutate(
+    freq = gsub("\\.", ",", freq) %>% paste("%", sep = ""),
+    label = str_c(n, " (", freq, ")") %>% str_squish()
+  )
+
+ggplot(sexo) +
+  aes(x = fct_reorder(SEXO, n, .desc=T), y = n, label = label) +
+  geom_bar(stat = "identity", fill = "#A11D21", width = 0.7) +
+  geom_text(
+    position = position_dodge(width = .9),
+    vjust = -0.5, #hjust = .5,
+    size = 3
+  ) + 
+  expand_limits(y = c(0, 30)) +
+  labs(x = "Sexo", y = "Frequência") +
+  theme_estat()
+ggsave("colunas-uni-sexo-febap.pdf", width = 158, height = 93, units = "mm")
