ab813714349a54813b146ab7582b5ed9ce0950d5
Lucas Fonseca Alves <lukitasdaestatistica@gmail.com>
2023-11-16 21:05:12 -0300

    Gráfico análise variável comorbidade do banco LASOS - OBSTETRÍCIA

--- rdocs/Lucas.R
+++ rdocs/Lucas.R
@@ -22,3 +22,39 @@ source("rdocs/source/packages.R")
 # de teste depreciados, ou ao menos deixando como comentário. Dê preferência
 # as funções dos pacotes contidos no Tidyverse para realizar suas análises.
 # ---------------------------------------------------------------------------- #
+
+caminho_lucas <- "resultados"
+
+
+banco_LASOS <- read_excel("banco/banco LASOS.xlsx", 
+                          sheet = "LASOS - OBSTETRÍCIA")
+
+
+comorb_obs <- banco_LASOS %>%
+  filter(!is.na(COMORBIDADES)) %>%
+  count(COMORBIDADES) %>%
+  mutate(
+    freq = n %>% percent(),
+  ) %>%
+  mutate(
+    freq = gsub("\\.", ",", freq) %>% paste("%", sep = ""),
+    label = str_c(n, " (", freq, ")") %>% str_squish()
+  )
+
+ggplot(comorb_obs) +
+  aes(
+    x = fct_reorder(COMORBIDADES, n, .desc = T),
+    y = n,
+    label = label
+  ) +
+  geom_bar(stat = "identity", fill = "#A11D21", width = 0.7) +
+  geom_text(
+    position = position_dodge(width = .9),
+    vjust = -0.5, # hjust = .5,
+    size = 3
+  ) +
+  labs(x = "Comorbidade", y = "Frequência") +
+  theme_estat()
+
+ggsave(filename = file.path(caminho_lucas, "colunas-uni-freq-comorb-obstetricia.pdf"), width = 158, height = 93, units = "mm")
+
